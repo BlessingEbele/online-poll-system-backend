27fcaa2761c1f50f2cfc8197fb449c1ae6380002
blessingebele <blessmart.com@gmail.com>
2025-09-27 02:51:57 +0100

    updated the auth_api/ulrs.py, views.py, serializers.py and maing project urls.py

--- auth_api/urls.py
+++ auth_api/urls.py
@@ -1,9 +1,9 @@
 # auth_api/urls.py
 from django.urls import path, include
-from .views import RegisterView,  LoginView, LogoutView
+from .views import UserRegisterSerializer,  LoginView, LogoutView
 
 urlpatterns = [
-    path("register/", RegisterView.as_view(), name="register"),
+    path("register/", UserRegisterSerializer.as_view(), name="register"),
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
 
