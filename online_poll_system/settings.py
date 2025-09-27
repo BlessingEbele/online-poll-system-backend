e08fbc5fd63b35f7e732ae42e71c73a56704d092
blessingebele <blessmart.com@gmail.com>
2025-09-27 15:13:56 +0100

    updated the settings.py

--- online_poll_system/settings.py
+++ online_poll_system/settings.py
@@ -37,7 +37,7 @@ if "runserver" in sys.argv:
     env.read_env(BASE_DIR / ".env.dev")
 else:
     # Production (PythonAnywhere)
-    env.read_env(BASE_DIR / ".env.prod")
+    env.read_env()
 
 # ------------------------------------------------------------------------------
 # General settings
